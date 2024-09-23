8c4772bf10b82dc88770f484cd745c7d8ef66111
Vaibhav-56 <vaibhavbhadauria15@gmail.com>
2024-09-23 05:43:13 -0700

    Add files via upload

---
+++ .c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main ()
+{
+	/*char a;
+printf("enter character\n");
+scanf("%c",&a);
+if(a=='a'){
+	printf("true");
+
+}else if (a=='e'){
+	printf("true");
+}else if (a=='i'){
+	printf("true");
+}else if (a=='o'){
+	printf("true");
+	
+}else if (a=='u'){
+	printf("true");
+}else {
+	printf("false");
+}*/
+
+int  a = 1;
+while (a<=100){
+	printf("%d\n",a);
+	a++;
+}
+if(a = 7){
+
+}	break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
